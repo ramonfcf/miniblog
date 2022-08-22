53fad5789d02fbe597bff41980fe2418f5b23f8d
ramonfcf <ramonfcf@gmail.com>
2022-08-22 08:38:15 -0300

    ✨ Adding react router and first pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import { BrowserRouber, Routes, Route, Navigate, BrowserRouter } from 'react-router-dom';
+
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
 
 function App() {
   return (
     <div className="App">
-      <h1>MiniBlog</h1>
+      <BrowserRouter>
+        <div className='countainer'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
