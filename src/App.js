e57bd96da445b6b0b86652ab79dc66c7f2f66617
ramonfcf <ramonfcf@gmail.com>
2022-08-24 07:57:44 -0300

    👽️ Creating inicial navigation

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import { BrowserRouber, Routes, Route, Navigate, BrowserRouter } from 'react-rou
 
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <div className='countainer'>
+        <div className='container'>
+          <Navbar />
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
           </Routes>
         </div>
+          <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <div>
+      <h3>Escreva sobre o que você tem interesse</h3>
+      <p>Mini Blog &copy; 2022</p>
+    </div>
+  )
+}
+
+export default Footer
