eda253ce9ff71232e174d93fb8b3058d1c796305
ramonfcf <ramonfcf@gmail.com>
2022-08-24 08:24:37 -0300

    🚧 Inicital style for navbar and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import styles from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.footer}>
       <h3>Escreva sobre o que você tem interesse</h3>
       <p>Mini Blog &copy; 2022</p>
     </div>
