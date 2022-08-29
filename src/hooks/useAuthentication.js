f79a7167c0a747cccc3394d36777e2c824996a05
ramonfcf <ramonfcf@gmail.com>
2022-08-29 08:43:32 -0300

    ⚡️ Creating a hook for user authentication

---
+++ src/hooks/useAuthentication.js
@@ -0,0 +1,24 @@
+import {
+   getAuth,
+   createUserWithEmailAndPassword,
+   signInWthEmailAndPassword,
+   updateProfile,
+   signOut
+} from 'firebase/auth'
+
+import { useState, useEffect } from 'react'
+
+export const useAuthentication = () => {
+   const [error, setError] = useState(null)
+   const [loading, setLoading] = useState(null)
+
+   const [cancelled, setCancelled] = useState(false)
+
+   const auth = getAuth()
+
+   function checkIfIsCancelled() {
+      if(cancelled){
+         return
+      }
+   }
+}
