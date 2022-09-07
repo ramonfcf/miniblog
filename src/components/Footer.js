57d296cd7347149728e8a570a2f7e871beaed919
ramonfcf <ramonfcf@gmail.com>
2022-09-07 07:47:11 -0300

    🏷️ Adding non authenticated pages block and changes to about

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <h3>Escreva sobre o que você tem interesse</h3>
+      <h3>Write about anything</h3>
       <p>Mini Blog &copy; 2022</p>
     </div>
   )
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,8 +1,16 @@
+import { Link } from 'react-router-dom'
 import styles from './About.module.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className={styles.about}>
+      <h2>About the Mini <span>Blog</span></h2>
+      <p>
+        This project was created using React in the Front-End and Firebase in the Back-End.</p>
+      <Link to="/posts/create" className='btn'>
+        Create post
+      </Link>
+    </div>
   )
 }
 
