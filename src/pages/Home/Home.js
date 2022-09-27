ac21b58f6442f9390a8517f112dc0346f7a7da65
ramonfcf <ramonfcf@gmail.com>
2022-09-27 08:18:51 -0300

    🚧 Preparing home page to recive posts

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,33 @@
 import styles from './Home.module.css'
+import { useNavigate, Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const Home = () => {
+  const [query, setQuery] = useState('')
+  const [posts] = useState([])
+
+  const handleSubtmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Here you can see the last users posts</h1>
+      <form onSubmit={handleSubtmit} className={styles.search_form}>
+        <input type="text" placeholder='search for tags...' onChange={(e) => setQuery(e.target.value)} />
+        <button className="btn btn-dark">Search</button>
+      </form>
+
+      <div>
+        <h2>Posts</h2>
+        {posts && posts.length === 0 && (
+          <div className={styles.noPosts}>
+            <h3>No posts yet</h3>
+            <Link to='/posts/create' className='btn'>Create one</Link>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
