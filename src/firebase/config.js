4d9d46261570aea4cfd83c26ead41144aabd8fdc
ramonfcf <ramonfcf@gmail.com>
2022-10-30 07:23:10 -0300

    🚑 Env fix

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,15 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseKeys = process.env.FIREBASE_KEYS;
-
 const firebaseConfig = {
-  apiKey: firebaseKeys.apiKey,
-  authDomain: firebaseKeys.authDomain,
-  projectId: firebaseKeys.projectId,
-  storageBucket: firebaseKeys.storageBucket,
-  messagingSenderId: firebaseKeys.messagingSenderId,
-  appId: firebaseKeys.appId,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_API_KEY,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
